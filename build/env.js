a48bc4d938f49e214a49a7f97ec484576af5cc94
rross3 <rross@averoinc.com>
2020-03-18 09:59:10 -0600

    Removing build file

--- build/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ENV = {
-    RUN_PORT: process.env.RUN_PORT || '5000',
-    ENVIRONMENT: process.env.ENVIRONMENT || 'dev',
-    POSTGRES_DATABASE_URL: `postgres://rross:2019Ianyfm@marketplace-catalog-dev.cjfkccsro3vy.us-east-1.rds.amazonaws.com/productCatalog`
-};
