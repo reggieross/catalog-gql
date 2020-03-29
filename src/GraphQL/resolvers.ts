f0000a159a4e0975c243554b0955b5a04bb3d987
Reggie Ross <reggieross@Reggies-MacBook-Pro.local>
2020-03-29 11:43:41 -0600

    Adding husky so we don't push broken builds

--- src/GraphQL/resolvers.ts
+++ src/GraphQL/resolvers.ts
@@ -4,7 +4,7 @@ import { Resolvers } from "./generated/resolvers";
 export const resolvers: Resolvers<Context> = {
   Query: {
     getProducts: async (_, { input }, ctx) => {
-      const products = await ctx.productService.getProductsForUser();
+      const products = await ctx.catalogService.getProductsForUser();
       return { products };
     }
   }
